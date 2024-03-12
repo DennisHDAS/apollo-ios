3552837d00de991af8ce3c271698e84873990dc7
Dima Hapich <dennish@dasxr.ai>
2024-03-12 17:30:29 +0200

    support VisionOS

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
     .iOS(.v12),
     .macOS(.v10_14),
     .tvOS(.v12),
-    .watchOS(.v5)
+    .watchOS(.v5),
+    .visionOS(.v1)
   ],
   products: [
     .library(name: "Apollo", targets: ["Apollo"]),
@@ -22,7 +23,7 @@ let package = Package(
   ],
   dependencies: [
     .package(
-      url: "https://github.com/stephencelis/SQLite.swift.git",
+      url: "https://github.com/DennisHDAS/SQLite.swift.git",
       .upToNextMajor(from: "0.13.1")),
   ],
   targets: [
